8fd604d79f5a968912c42302cd77587d92a682b6
Leonardo Pignataro <leopignataro@gmail.com>
2017-06-20 02:53:08 -0300

    Locale selector

--- template/styles/main.js
+++ template/styles/main.js
@@ -1,1 +1,26 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
+$(function () {
+
+	var currentLocale = null;
+	var pathAfterLocale = null;
+
+	var init = function () {
+		var m = /^\/(\w{2}-\w{2})\/(.*)$/.exec(document.location.pathname);
+		if (m) {
+			currentLocale = m[1];
+			pathAfterLocale = m[2];
+			$('#locale-picker').val(currentLocale);
+			$('#locale-picker').change(localeSelectionChanged);
+		} else {
+			console.log('Could not determine current locale');
+		}
+	};
+
+	var localeSelectionChanged = function (e) {
+		var newLocale = $(e.target).val();
+		document.location = '/' + newLocale + '/' + pathAfterLocale;
+	};
+
+	init();
+
+});
